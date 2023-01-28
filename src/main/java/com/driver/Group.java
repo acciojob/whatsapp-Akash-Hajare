d96cdd46e87426c5a7c8591f5e1feac7b56fff67
Akash-Hajare <akashhajare97@gmail.com>
2023-01-28 18:35:21 +0530

    initial commit

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -1,7 +1,49 @@
 package com.driver;
 
+import java.util.List;
+
 public class Group {
     private String name;
     private int numberOfParticipants;
+    private List<User> userList;
+    private User admin;
 
+    public Group(String name, int numberOfParticipants, List<User> userList, User admin) {
+        this.name = name;
+        this.numberOfParticipants = numberOfParticipants;
+        this.userList = userList;
+        this.admin = admin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfParticipants() {
+        return numberOfParticipants;
+    }
+
+    public void setNumberOfParticipants(int numberOfParticipants) {
+        this.numberOfParticipants = numberOfParticipants;
+    }
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+    }
+
+    public User getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(User admin) {
+        this.admin = admin;
+    }
 }
